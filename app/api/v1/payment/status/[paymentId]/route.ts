4a5ef117e94d1e3195133ebacfb6661d882ba4bb
Gustavo Da Nóbrega Brandão <gustavonobg@gmail.com>
2025-06-07 10:31:32 -0300

    fix: bug build vps

--- app/api/v1/payment/status/[paymentId]/route.ts
+++ app/api/v1/payment/status/[paymentId]/route.ts
@@ -2,10 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 const API_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export async function GET(req: NextRequest, context: { params: { paymentId: string } }) {
+export async function GET(req: NextRequest, context: any) {
   try {
-    const { params } = context;
-    const { paymentId } = await context.params;
+    const { paymentId } = context.params;
 
     const response = await fetch(`${API_URL}/api/v1/payment/status/${paymentId}`, {
       method: "GET",
@@ -13,7 +12,7 @@ export async function GET(req: NextRequest, context: { params: { paymentId: stri
 
     const data = await response.json();
 
-    console.log("Statusdopagamento",data);
+    console.log("Statusdopagamento", data);
 
     if (!response.ok) {
       return NextResponse.json({ error: data.message || "Erro ao verificar status" }, { status: response.status });
